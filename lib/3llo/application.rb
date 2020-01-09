1308bba2b1cccd9f9613787ad036e82ff21dafed
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-01-09 20:01:15 +0100

    Introduce config file

--- lib/3llo/application.rb
+++ lib/3llo/application.rb
@@ -2,24 +2,33 @@ module Tr3llo
   module Application
     extend self
 
+    DEFAULT_CONFIG_FILE_PATH = "~/.3llo.config.json"
+
     def start(args)
       $container = Container.new()
 
       interface = register_interface!()
+      option_parser = build_option_parser()
+      register_api_client!()
 
-      options = parse_cli_args!(args)
+      options = parse_cli_args!(option_parser, args)
 
       if options.key?(:help)
-        print_help!(fetch_interface!())
+        execute_help!(option_parser, interface)
+        exit
+      end
+
+      if options.key?(:configure)
+        execute_configure!(interface)
         exit
       end
 
       init_command = options.fetch(:init, "")
+      config_file = options.fetch(:config, DEFAULT_CONFIG_FILE_PATH)
 
       print_help!(interface)
 
-      register_api_client!()
-      load_configuration!()
+      load_configuration!(config_file, interface)
       register_registry!()
 
       load_user!(interface)
@@ -27,21 +36,49 @@ module Tr3llo
       Controller.start(init_command)
     end
 
-    def print_help!(interface)
-      Presenter::HelpPresenter.print!(interface)
+    def execute_help!(parser, interface)
+      interface.puts(parser.help)
     end
 
-    def parse_cli_args!(args)
-      options = {}
+    def execute_configure!(interface)
+      interface.puts("Please key in the following information:")
+      interface.puts("Tips: Visit https://trello.com/app-key to obtain the credentials.\n\n")
 
-      OptionParser.new do |parser|
-        parser.on("-h", "--help", "Display this help message")
-        parser.on("-i", "--init=INIT", String, "The init command")
-      end.parse!(args, into: options)
+      key = interface.input.ask("API key:", required: true)
+      token = interface.input.ask("API token:", required: true)
+
+      if API::Token.verify(key, token)
+        file_content = JSON.dump({"key" => key, "token" => token})
+        File.write(File.expand_path(DEFAULT_CONFIG_FILE_PATH), file_content)
+
+        message = "Configuration has been saved to" + DEFAULT_CONFIG_FILE_PATH.green + "."
+        interface.puts(message)
+      else
+        error_message = "Either key or token is invalid. Please try again."
+        interface.puts(error_message.red)
+      end
+    end
+
+    def parse_cli_args!(parser, args)
+      options = {}
+      parser.parse!(args, into: options)
 
       options
     rescue OptionParser::InvalidArgument, OptionParser::InvalidOption => exception
       {}
+    end
+
+    def build_option_parser()
+      OptionParser.new do |parser|
+        parser.on("-h", "--help", "show this message")
+        parser.on("-i", "--init=INIT", String, "the init command to run when the program starts")
+        parser.on("-c", "--config=CONFIG", String, "file path to the config file")
+        parser.on("--configure", "set up configuration")
+      end
+    end
+
+    def print_help!(interface)
+      Presenter::HelpPresenter.print!(interface)
     end
 
     def register_api_client!()
@@ -53,18 +90,41 @@ module Tr3llo
       $container.register(:interface, Tr3llo::Interface.new(prompt, $stdout))
     end
 
-    def load_configuration!()
+    def load_configuration!(config_file, interface)
+      config_path = File.expand_path(config_file)
+
+      config =
+        if File.exists?(config_path)
+          JSON.load(File.read(config_path))
+        else
+          {}
+        end
+
       configuration = Tr3llo::Configuration.new
-      begin
-        configuration.api_key = ENV.fetch('TRELLO_KEY') { raise "Have you set TRELLO_KEY?" }
-        configuration.api_token = ENV.fetch('TRELLO_TOKEN') { raise "Have you set TRELLO_TOKEN?" }
-      rescue => error
-        abort "Invalid configuration: \e[1m#{error.message}".colorize(31)
-      end
+
+      configuration.api_key = get_config_entry(config, "key", "TRELLO_KEY")
+      configuration.api_token = get_config_entry(config, "token", "TRELLO_TOKEN")
 
       configuration.finalize!()
 
       $container.register(:configuration, configuration)
+    rescue KeyError => exception
+      command_string = "3llo --configure"
+      abort "#{exception.key.inspect} has not been configured. Please run #{command_string.inspect} to set up configuration.".red
+    end
+
+    def get_config_entry(config, key, env_key)
+      config.fetch(key) do
+        if ENV.has_key?(env_key)
+          Utils.deprecate!(
+            "Setting #{env_key.inspect} as an environment variable is deprecated. It will be removed in the future versions of 3llo. Please use config file instead."
+          )
+
+          ENV.fetch(env_key)
+        else
+          raise KeyError.new(key: key)
+        end
+      end
     end
 
     def register_registry!()
--- lib/3llo/controller.rb
+++ lib/3llo/controller.rb
@@ -16,22 +16,22 @@ module Tr3llo
       if init_command && init_command != ""
         interface.puts("Executing " + init_command.yellow + " command")
 
-        execute_command!(init_command)
+        execute_command!(init_command, interface)
       end
 
       loop do
         command_buffer = Readline.readline("\e[15;48;5;27m 3llo \e[0m > ", true)
 
-        execute_command!(command_buffer)
+        execute_command!(command_buffer, interface)
       end
     rescue Interrupt
       Command::ExitCommand.execute()
     end
 
-    def execute_command!(command_buffer)
+    def execute_command!(command_buffer, interface)
       Tr3llo::CommandFactory.execute(command_buffer.strip())
     rescue Tr3llo::HTTP::Client::RequestError => e
-      interface.print_frame { interface.puts(e.message) }
+      interface.print_frame { interface.puts(e.message.red) }
     end
   end
 end
