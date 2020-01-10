a4158216ea8b0186cb2052586bbe9414e7da7ce1
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-01-10 15:04:33 +0100

    Fetch cards concurrently

--- lib/3llo/interface.rb
+++ lib/3llo/interface.rb
@@ -14,6 +14,7 @@ module Tr3llo
     def print_line(str)
       output.puts(str)
     end
+
     alias :puts :print_line
 
     def print(str)
--- lib/3llo/registry.rb
+++ lib/3llo/registry.rb
@@ -4,32 +4,35 @@ module Tr3llo
 
     def initialize
       @store = {}
+      @semaphore = Mutex.new()
     end
 
     def register(type, id)
-      data =
-        @store.fetch(type, {
-          counter: 0,
-          id_to_shortcut: {},
-          shortcut_to_id: {}
-        })
+      @semaphore.synchronize do
+        data =
+          @store.fetch(type, {
+            counter: 0,
+            id_to_shortcut: {},
+            shortcut_to_id: {}
+          })
 
-      id_to_key = data[:id_to_shortcut]
-      key_to_id = data[:shortcut_to_id]
+        id_to_key = data[:id_to_shortcut]
+        key_to_id = data[:shortcut_to_id]
 
-      if id_to_key.key?(id)
-        id_to_key.fetch(id)
-      else
-        counter = data[:counter] + 1
-        shortcut = counter.to_s
+        if id_to_key.key?(id)
+          id_to_key.fetch(id)
+        else
+          counter = data[:counter] + 1
+          shortcut = counter.to_s
 
-        data[:counter] = counter
-        data[:id_to_shortcut][id] = shortcut
-        data[:shortcut_to_id][shortcut] = id
+          data[:counter] = counter
+          data[:id_to_shortcut][id] = shortcut
+          data[:shortcut_to_id][shortcut] = id
 
-        @store[type] = data
+          @store[type] = data
 
-        shortcut
+          shortcut
+        end
       end
     end
 
